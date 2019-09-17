0774c0c4f7281bf974e779c8640fcaf5fee85bba
Hugo Larrousse <hugo.larrousse@gmail.com>
2019-09-17 10:58:57 +0200

    add socket client (connected to core)

--- socket.js
+++ socket.js
@@ -1,5 +1,26 @@
+const ioH7 = require('socket.io-client');
+
 const { io } = require('./server');
 const timeout = require('./src/utils');
+const managePi = require('./src/managePi');
+
+
+exports.initializeSocketH7 = (serial) => {
+  const socketH7 = ioH7.connect('https://sockets.staging.heptaward.com', { secure: true, reconnect: true });
+
+  socketH7.on('connect', () => {
+    setTimeout(() => {
+      socketH7.emit('newPi', serial);
+    }, 1000);
+  });
+
+  socketH7.on('need_reboot_pi', async (serialToReboot) => {
+    console.log('serial inside reboot', serial);
+    if (serialToReboot === serial) {
+      managePi.needReboot();
+    }
+  });
+};
 
 let socketGlobal = null;
 
