a2357165d0395dbbf6526151bc64faf055b36bf8
Hugo Larrousse <hugo.larrousse@gmail.com>
2019-10-03 17:35:23 +0200

    fix cec controller

--- src/cec.js
+++ src/cec.js
@@ -1,13 +1,20 @@
 const CecController = require('cec-controller');
 
+const findTVController = (controller) => {
+  if (controller && controller.dev4 && controller.dev4.name === 'TV') return 4;
+  if (controller && controller.dev0 && controller.dev0.name === 'TV') return 0;
+  return null;
+};
+
 exports.stop = () => new Promise((resolve, reject) => {
   const cecCtl = new CecController();
   cecCtl.on('ready', async (controller) => {
     if (!controller) {
       return reject();
     }
-    if (controller.dev4) {
-      await controller.dev4.turnOff();
+    const numberDev = findTVController(controller);
+    if (numberDev) {
+      await controller[`dev${numberDev}`].turnOff();
     }
     return resolve();
   });
@@ -23,8 +30,9 @@ exports.start = () => new Promise((resolve, reject) => {
     if (!controller) {
       return reject();
     }
-    if (controller.dev4) {
-      await controller.dev4.turnOn();
+    const numberDev = findTVController(controller);
+    if (numberDev) {
+      await controller[`dev${numberDev}`].turnOn();
     }
     return resolve();
   });
@@ -37,7 +45,7 @@ exports.start = () => new Promise((resolve, reject) => {
 exports.test = () => new Promise((resolve, reject) => {
   const cecCtl = new CecController();
   cecCtl.on('ready', (controller) => {
-    resolve(controller && controller.dev4);
+    resolve(findTVController(controller));
   });
   cecCtl.on('error', (error) => {
     console.log(error);
